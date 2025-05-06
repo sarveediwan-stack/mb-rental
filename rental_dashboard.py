bcd461bb632ec3bd6b7f057f6d2049ddd150b17d
sarveediwan-stack <sarvee@propico.in>
2025-05-06 16:49:40 +0530

    Update rental_dashboard.py

--- rental_dashboard.py
+++ rental_dashboard.py
@@ -3,6 +3,7 @@ from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import shap
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
